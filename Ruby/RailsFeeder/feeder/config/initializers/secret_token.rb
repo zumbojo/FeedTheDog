0d3530f145f98b6d43cf69b590e4e39d6b69265a
zumbojo <zumbojo@gmail.com>
2011-08-08 19:36:33 -0500

    rails new feeder

---
+++ Ruby/RailsFeeder/feeder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feeder::Application.config.secret_token = '5bbc5c12b9000ab93d833c5c7d308f09ced3f4b1b07f79a01bab6cb1accc7dc0020350021078aded42048499551e5c820952e5d1491464b3ae04bf59534bf723'
