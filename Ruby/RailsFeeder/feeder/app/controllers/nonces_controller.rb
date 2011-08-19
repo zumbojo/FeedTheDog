be48e9cf0878e497cb3a24e356ebf90dc16abf6a
zumbojo <zumbojo@gmail.com>
2011-08-18 20:37:34 -0500

    POST lookup test working.

--- Ruby/RailsFeeder/feeder/app/controllers/nonces_controller.rb
+++ Ruby/RailsFeeder/feeder/app/controllers/nonces_controller.rb
@@ -1,16 +1,19 @@
 class NoncesController < ApplicationController
   def create
-    @nonce = Nonce.create
-    if @nonce.save
-      render :text => @nonce.unsigned_nonce
+    nonce = Nonce.create
+    if nonce.save
+      render :text => nonce.unsigned_nonce
     else
       render :text => "failure"
     end
   end
 
   def use
-    render :text => "failure"
+    # render :text => params[:signed_nonce]
+    # render :text => "failure"
 
+    nonce = Nonce.find_by_signed_nonce(params[:signed_nonce])
+    render :text => nonce.unsigned_nonce
     # pseudo:
     # accept signed_nonce from browser
     # check db for signed_nonce, where used date is nil
--- Ruby/RailsFeeder/feeder/app/models/nonce.rb
+++ Ruby/RailsFeeder/feeder/app/models/nonce.rb
@@ -1,17 +1,17 @@
 class Nonce < ActiveRecord::Base
 
-  after_initialize :generate_values
+  before_save :generate_values
 
   private
 
     def generate_values
-      @unsigned = ''
-      32.times { @unsigned << ('a'..'z').to_a[rand(26)] }
-      self.unsigned_nonce = @unsigned
+      unsigned = ''
+      32.times { unsigned << ('a'..'z').to_a[rand(26)] }
+      self.unsigned_nonce = unsigned
 
-      @private_key = "pnwmrpjitekvmoyhrjkgbdcrghxkgqzc"
+      private_key = "pnwmrpjitekvmoyhrjkgbdcrghxkgqzc"
         # todo: roll a new key, put in non-versioned file
-      self.signed_nonce = Digest::SHA2.hexdigest(@unsigned + @private_key)
+      self.signed_nonce = Digest::SHA2.hexdigest(unsigned + private_key)
     end
 
 end
