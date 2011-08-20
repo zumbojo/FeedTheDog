af785aefd723f5ead17e96c793fb858410edb6df
zumbojo <zumbojo@gmail.com>
2011-08-20 14:48:13 -0500

    NSUserDefaults stuff.  Untested.

--- iOS/FeedTheDog/FeedTheDog/Feeder.h
+++ iOS/FeedTheDog/FeedTheDog/Feeder.h
@@ -9,5 +9,15 @@
 #import <Foundation/Foundation.h>
 
 @interface Feeder : NSObject
+{
+    NSURL *url;
+    NSString *key;
+}
+
+@property (nonatomic, retain) NSURL *url;
+@property (nonatomic, retain) NSString *key;
+
++ (Feeder *)sharedFeeder;
+- (void)saveUrlString:(NSString *)urlString andPrivateKey:(NSString *)privateKey;
 
 @end
--- iOS/FeedTheDog/FeedTheDog/Feeder.m
+++ iOS/FeedTheDog/FeedTheDog/Feeder.m
@@ -6,11 +6,37 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+#define URL_STRING_DEFAULTS_KEY @"urlString"
+#define PRIVATE_KEY_DEFAULTS_KEY @"privateKey"
+
 #import "Feeder.h"
 #import "SynthesizeSingleton.h"
 
 @implementation Feeder
 
 SYNTHESIZE_SINGLETON_FOR_CLASS(Feeder);
+
+@synthesize url;
+@synthesize key;
+
+#pragma mark NSUserDefaults
+- (void)loadDefaults
+{
+    url = [NSURL URLWithString:[[NSUserDefaults standardUserDefaults] stringForKey:URL_STRING_DEFAULTS_KEY]];
+    key = [[NSUserDefaults standardUserDefaults] stringForKey:PRIVATE_KEY_DEFAULTS_KEY];
+}
+
+- (void)saveUrlString:(NSString *)urlString andPrivateKey:(NSString *)privateKey;
+{
+    // update existing:
+    url = [NSURL URLWithString:urlString];
+    key = [NSString stringWithString:privateKey];
+    
+    // save
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setValue:urlString forKey:URL_STRING_DEFAULTS_KEY];
+    [defaults setValue:privateKey forKey:PRIVATE_KEY_DEFAULTS_KEY];
+    [defaults synchronize];
+}
 
 @end
