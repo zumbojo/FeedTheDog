c0cd08c166ff3ea6ae11ebd9eb4b41c0efb92082
zumbojo <zumbojo@gmail.com>
2011-08-20 11:29:02 -0500

    Serial connection working - feeder accepts signed_nonces and activates the Arduino.  VERY NICE.

--- Ruby/RailsFeeder/feeder/app/controllers/nonces_controller.rb
+++ Ruby/RailsFeeder/feeder/app/controllers/nonces_controller.rb
@@ -31,8 +31,18 @@ class NoncesController < ApplicationController
   private
     
     def activate_arduino
+      # port_str = "/dev/tty.usbserial-A900cesN"  # MacBook
+      port_str = "/dev/ttyUSB0"  # kitchen ubuntu box
+      baud_rate = 9600
+      data_bits = 8
+      stop_bits = 1
+      parity = SerialPort::NONE
 
+      sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+
+      sp.putc(1);
+
+      sp.close
     end
-
 
 end
