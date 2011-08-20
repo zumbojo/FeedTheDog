628e0a05c4f08bd4dae4214c88f2f5c5d8191731
zumbojo <zumbojo@gmail.com>
2011-08-20 13:59:15 -0500

    Creating Feeder singleton.

---
+++ iOS/FeedTheDog/FeedTheDog/Feeder.h
@@ -0,0 +1,13 @@
+//
+//  Feeder.h
+//  FeedTheDog
+//
+//  Created by user on 8/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Feeder : NSObject
+
+@end
---
+++ iOS/FeedTheDog/FeedTheDog/Feeder.m
@@ -0,0 +1,23 @@
+//
+//  Feeder.m
+//  FeedTheDog
+//
+//  Created by user on 8/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Feeder.h"
+
+@implementation Feeder
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
